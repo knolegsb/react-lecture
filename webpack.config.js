1321386899d0bff244b36f8bdc71b91c085f98ec
Sean You <syou@netgnix.com>
2017-03-28 17:04:35 -0700

    added webpack.config

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    entry: './src/index.js',
+
+    output: {
+        path: __dirname,
+        filename: 'app.js'
+    },
+
+    devServer: {
+        inline: true,
+        port: 7777
+    },
+
+    module:{
+        loaders:[
+            {
+                test: /\.js$/,
+                loader: 'babel',
+                exclude: /node_modules/,
+                query: {
+                    cacheDirectory: true,
+                    preset: ['es2015', 'react']
+                }
+            }
+        ]
+    }
+};
